e0705b2d242b41d04b6079b0c21c8e9b720be7be
Lee Ka Xiang <>
2024-11-11 18:04:42 +0800

    udate

--- frontend/screens/LoginScreen.tsx
+++ frontend/screens/LoginScreen.tsx
@@ -9,7 +9,7 @@ const LoginScreen = ({ navigation }: any) => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('https://daily-cash-flow-app.vercel.app/api/login', {
+      const response = await fetch('https://cash-flow-app-blush.vercel.app/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
